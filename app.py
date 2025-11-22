c1f265638a0a53db5053705811f2473192972bb5
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-11-23 07:21:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ html_content = html_template.replace(
     f"const allMarkdownData = {markdown_json};"
 )
 
-components.html(html_content, height=800, width=1200, scrolling=True)
+components.html(html_content, height=800, width=None, scrolling=True)
